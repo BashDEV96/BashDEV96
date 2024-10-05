b2bf49b64f18ef45290e089e08e917bf65eb0a5f
Greg <justintime7469@gmail.com>
2024-10-05 08:59:28 -0500

    Added a new echo line to first_script.sh

--- first_script.sh
+++ first_script.sh
@@ -3,3 +3,4 @@
 echo "Hello, World!"
 echo "This is BashDEV96's first script on GitHub!"
 echo "The journey of a thousand commits begins with a single script."
+echo "This is my first Git commit from my local machine!"
